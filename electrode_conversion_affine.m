31696f1a151e442978d0dd5524818f59adee2424
dpachec <dapachec@gmail.com>
2024-11-26 11:49:50 +0100

    update

--- electrode_conversion_affine.m
+++ electrode_conversion_affine.m
@@ -4,17 +4,6 @@
 clear
 mainPath = 'D:/Appartment/extract_electrodes/';
 
-%% create empty folders for N number of subjects
-
-numSubj = 27; 
-
-for subji = 1:numSubj
-    
-    string_path = [mainPath 's' num2str(subji, '%02d')]; 
-    mkdir(string_path);
-
-end
-
 %% each folder needs to contain 
 % 1) MRI in ACPC coordinates (s01_T1_acpc.nii), generated in 3D slicer
 % 2) Raw post CT scan (s01_CT.nii.gz)
@@ -27,11 +16,11 @@ cd ([mainPath subjID]);
 
 %% convert fiducias in acpc space to MNI using the affine transform
 % read xfm of this particular subject
-xfm = read_talxfm([mainPath subjID '/talairach.xfm'])
+xfm = read_talxfm([mainPath subjID '/freesurfer/mri/transforms/talairach.xfm'])
 
 
 %% read fiducials
-fiducials = readtable('s01_fid_acpc.csv'); 
+fiducials = readtable('s01_fiducials.csv'); 
 elec_acpc = table2array(fiducials(:, 2:4))
 
 %% apply transform
@@ -51,7 +40,7 @@ elec_mni_frv.unit = 'mm';
 load('D:/Documents/MATLAB/fieldtrip-master/template/anatomy/surface_pial_left.mat');
 figure
 ft_plot_mesh(mesh, 'facealpha', .5); 
-ft_plot_sens(elec_mni_frv, 'elecsize', 100, 'facecolor', [1 0 0]);
+ft_plot_sens(elec_mni_frv, 'elecsize', 20, 'facecolor', [1 0 0]);
 %ft_plot_sens(elec_acpc_f, 'elecsize', 100);
 view([-55 10]);
 material dull; 
@@ -73,7 +62,7 @@ elec_acpc_frv.unit = 'mm';
 pial_lh = ft_read_headshape([mainPath subjID '/freesurfer/surf/lh.pial.T1']);
 pial_lh.coordsys = 'acpc';
 ft_plot_mesh(pial_lh, 'facealpha', .5); 
-ft_plot_sens(elec_acpc_frv, 'elecsize', 100, 'facecolor', [1 0 0]);
+ft_plot_sens(elec_acpc_frv, 'elecsize', 30, 'facecolor', [1 0 0]);
 %ft_plot_sens(elec_acpc_f, 'elecsize', 100);
 view([-55 10]);
 material dull; 
--- spm_corregistration.m
+++ spm_corregistration.m
@@ -1,61 +1,84 @@
-%% Import the FreeSurfer-processed MRI
-fsmri_acpc = ft_read_mri([path subjID '/anat/freesurfer/mri/T1.mgz']);
-%fsmri_acpc = ft_read_mri([path subjID '/freesurfer/mri/T1.mgz']);
-fsmri_acpc.coordsys = 'acpc';
+%% 
+%% First align all subjects CT scans to CTF and then ACPC (this has to be done for all subjects before running the SPM loop below)
+clear, clc 
+globalPath = 'D:/Appartment/extract_electrodes/'; 
+
+subjID = 's01';
+
+%Import the anatomical CT
+ct = ft_read_mri([globalPath subjID '/' subjID '_CT.nii.gz']);
+%determine the native orientation of the anatomical CT’s > this is not necessary, all left 2 right
+% ft_determine_coordsys(ct, 'interactive', 'no') 
+% plot just to check
+%cfg = []; 
+%ft_sourceplot(cfg,ct);
+%cfg = []; 
+%ft_sourceplot(cfg,fsmri_acpc);
 
 
+%% Align the anatomical CT to the CTF head surface coordinate system > normally this should be done, so no loop, let's test without
+cfg = [];
+cfg.method = 'interactive';
+cfg.coordsys = 'ctf'; % use acpc if not possible with ctf
+ct_ctf = ft_volumerealign(cfg, ct);
+
+%% Convert CTF to ACPC and write the anatomical CT in ACPC coordinates out to a file 
+
+ct_acpc = ft_convert_coordsys(ct_ctf, 'acpc');
+cfg = [];
+cfg.filename = [globalPath subjID '/' subjID '_CT_acpc'];
+cfg.filetype = 'nifti';
+cfg.parameter = 'anatomy';
+ft_volumewrite(cfg, ct_acpc);
 
 
-%% Import the anatomical CT
-ct = ft_read_mri([path subjID '/anat/' subjID '_CT.nii.gz']);
+%% loop to corregister CTs and MR scans using SPM
+clear, clc
+
+globalPath = 'D:/Appartment/extract_electrodes/'; 
+%subjID = 's02'; 
+
+
+for subji = 1% [1:8 10:27] %subject 9 has 3 DICOM batches and is done in 3DSlicer
+
+    subjID = ['s' num2str(subji, '%02d')]; 
+
+    %Import the FreeSurfer-processed MRI
+    fsmri_acpc = ft_read_mri([globalPath subjID '/freesurfer/mri/T1.mgz']);
+    fsmri_acpc.coordsys = 'acpc';
+
+    %Import the CT in ACPC coordinates
+    ct_acpc = ft_read_mri([globalPath subjID '/' subjID '_CT_acpc.nii']);
+
+    
+    %%Fuse the CT with the MRI using the below command. ~ aprox 40s
+    
+    cfg = [];
+    cfg.method = 'spm';
+    cfg.spmversion = 'spm12';
+    cfg.coordsys = 'acpc';
+    cfg.viewresult = 'yes';
+    ct_acpc_f = ft_volumerealign(cfg, ct_acpc, fsmri_acpc);
+    
+    
+    
+    %%Write the MRI-fused anatomical CT out to a file 
+    tic 
+    
+    cfg = [];
+    cfg.filename = [globalPath subjID '/' subjID '_CT_acpc_f'];
+    cfg.filetype = 'nifti';
+    cfg.parameter = 'anatomy';
+    ft_volumewrite(cfg, ct_acpc_f);
+    
+    toc
+
+    
+
+
+end
+
+
 
 
 %%
-cfg = []; 
-ft_sourceplot(cfg,ct);
-cfg = []; 
-ft_sourceplot(cfg,fsmri_acpc);
-
-%% determine the native orientation of the anatomical CT’s
-ft_determine_coordsys(ct, 'interactive', 'no')
-%ft_determine_coordsys(ct, 'interactive', 'yes')
-
-
-%% Align the anatomical CT to the CTF head surface coordinate system
-
-cfg = [];
-cfg.method = 'interactive';
-cfg.coordsys = 'acpc'; % use acpc if not possible with ctf
-ct_ctf = ft_volumerealign(cfg, ct);
-
-%% convert the CT’s coordinate system into an approximation of the ACPC coordinate system
-%Fuse the CT with the MRI using the below command. ~ aprox 40s
-%Write the MRI-fused anatomical CT out to a file 
-
-tic
-
-ct_acpc = ft_convert_coordsys(ct_ctf, 'acpc');
-
-toc
-
-%% Fuse the CT with the MRI using the below command. ~ aprox 40s
-
-cfg = [];
-cfg.method = 'spm';
-cfg.spmversion = 'spm12';
-cfg.coordsys = 'acpc';
-cfg.viewresult = 'yes';
-ct_acpc_f = ft_volumerealign(cfg, ct_acpc, fsmri_acpc);
-
-
-
-%% Write the MRI-fused anatomical CT out to a file 
-tic 
-
-cfg = [];
-cfg.filename = [subjID '_CT_acpc_f'];
-cfg.filetype = 'nifti';
-cfg.parameter = 'anatomy';
-ft_volumewrite(cfg, ct_acpc_f);
-
-toc
