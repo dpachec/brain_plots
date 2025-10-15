274bf69a35aa3675c8e5483b7b0c1a02ef1e6c6f
Daniel Pacheco <dapachec@gmail.com>
2025-10-15 19:51:03 +0200

    Update mainPath and subject index in extract_labels.m
    Changed the mainPath variable to point to the iEEG_projects directory and updated the subject index loop from 30 to 35. This prepares the script to process data for subject 35.

--- extract_labels.m
+++ extract_labels.m
@@ -3,11 +3,11 @@
 
 clear, clc
 %mainPath = 'D:/Appartment/extract_electrodes/';
-%mainPath = '/Users/danielpacheco/Documents/iEEG_projects/Appartment/electrode_information/';
-mainPath = 'C:/Users/1764316/Desktop/electrode_location_remaining_subjects/'; 
+mainPath = '/Users/danielpacheco/Documents/iEEG_projects/Appartment/electrode_information/';
+%mainPath = 'C:/Users/1764316/Desktop/electrode_location_remaining_subjects/'; 
 
 
-for subji = 30:30
+for subji = 35:35
 
     subjID = ['s' num2str(subji, '%02d')];
     elecCSV = readtable([mainPath subjID '/' subjID '_fiducials.csv']);
